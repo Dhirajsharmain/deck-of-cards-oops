8bcc951789ed42e13183315cd1e48e5067c0caae
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-17 09:31:56 +0530

    Added: Setup the initial game with deck of cards and make sure we have unique cards

--- bridgelabz/DectOfCard.java
+++ bridgelabz/DectOfCard.java
@@ -1,4 +1,21 @@
 package bridgelabz;
 
 public class DectOfCard {
+    public static void main(String[] args) {
+        String[] cardsSuit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] cardsRank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+        // initialize deck
+        String[] deck = new String[52];
+        for (int i = 0; i < cardsRank.length; i++) {
+            for (int j = 0; j < cardsSuit.length; j++) {
+                deck[cardsSuit.length * i + j] = cardsRank[i] + " of " + cardsSuit[j];
+            }
+        }
+
+        for (String element:deck) {
+            System.out.println(element);
+        }
+
+    }
 }
